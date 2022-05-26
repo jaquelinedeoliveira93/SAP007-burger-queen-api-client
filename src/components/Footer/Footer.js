3ed4e4a6a8ebf7407b756ebaec374c686deaf4d7
Jaqueline de Oliveira <jaquelinedeoliveiraa@gmail.com>
2022-05-25 22:28:58 -0300

    docs: register e login com letra maiúscula

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer>
-      <p>Desenvolvido por <a className='linkFooter" href="https://github.com/jaquelinedeoliveira93'>Jaqueline de Oliveira</a></p>
+      <p>Desenvolvido por <a className="linkFooter" href="https://github.com/jaquelinedeoliveira93">Jaqueline de Oliveira</a></p>
     </footer>
   );
 };
