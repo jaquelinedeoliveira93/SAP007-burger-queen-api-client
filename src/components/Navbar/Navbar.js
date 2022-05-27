eee3cfb5052af1c21c185d135a2b0d33331aeb0f
Jaqueline de Oliveira <jaquelinedeoliveiraa@gmail.com>
2022-05-26 23:24:37 -0300

    refactor: melhorando a estrutura da Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,36 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import './Navbar.css';
 
 function NavBar() {
   return (
     <nav className='navegationBar'>
-      <Link to='/hall' className='linkNavbar'>Salão</Link>
-      <Link to='/kitchen' className='linkNavbar'>Cozinha</Link>
-      <Link to='/readyOrders' className='linkNavbar'>Pedidos prontos</Link>
-      <Link to='/' className='linkNavbar'>Sair</Link>
+      <span className='brand'>Burger Queen</span>
+      <ul className='linksUl'>
+        <li className='linkLi'>
+          <NavLink to='/hall' className='linkNavbar'>
+            Salão
+          </NavLink>
+        </li>
+        <li className='linkLi'>
+          <NavLink to='/kitchen' className='linkNavbar'>
+            Cozinha
+          </NavLink>
+        </li>
+        <li className='linkLi'>
+          <NavLink to='/readyOrders' className='linkNavbar'>
+            Pedidos prontos
+          </NavLink>
+        </li>
+        <li className='linkLi'>
+          <NavLink to='/' className='linkNavbar'>
+            Sair
+          </NavLink>
+        </li>
+      </ul>
     </nav>
   );
 };
 
 export default NavBar;
+
+
