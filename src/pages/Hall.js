b20ca18da83a3673efb45fa6b74368c284e831c9
Jaqueline de Oliveira <jaquelinedeoliveiraa@gmail.com>
2022-06-06 18:29:00 -0300

    feat: estrutura da página de pedidos e inicio da estilização

--- src/pages/Hall.js
+++ src/pages/Hall.js
@@ -1,12 +1,46 @@
+import './index.css';
+import './Hall.css';
+
 // import components
 import Navbar from '../components/Navbar/Navbar';
+import Button from '../components/Button/Button';
+import ButtonsMenu from '../components/Button/ButtonsMenu';
+import Input from '../components/Form/Input';
 
 function Hall() {
   return (
     <div>
       <Navbar />
-      <h1>PÁGINA DOS GARÇONS</h1>
-    </div>
+      <ButtonsMenu />
+      <section className='orderPad'>
+        <div className='backgroundTitleOrders'>
+          <p className='titleOrders'>PEDIDOS</p>
+        </div>
+        <div className='clientAndTable'>
+          <Input
+            className='inputCliente'
+            type='text'
+            label='Nome:'
+            name='name'
+          />
+
+          <Input
+            className='inputTable'
+            type='number'
+            label='Mesa:'
+            name='table'
+          />
+        </div>
+        <div className='totalAndButton'>
+          <p className='valueOrder'>TOTAL:</p>
+
+          <Button
+            className='confirmButton'
+            text='CONFIRMAR'
+          />
+        </div>
+      </section >
+    </div >
   );
 }
 
